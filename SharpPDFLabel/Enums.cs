f9791d0c8b8679c7d73b015ea35baa6f9c06560a
Shaikh Irshad <shaikhij2000@gmail.com>
2022-11-23 11:15:59 +0530

    Initial commit

---
+++ SharpPDFLabel/Enums.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpPDFLabel
+{
+    public class Enums
+    {
+        /// <summary>
+        /// The page size of the document
+        /// See the Switch statement in LabelCreator.CreatePDF() if you add a new one
+        /// </summary>
+        public enum PageSize
+        {
+            A4
+        }
+
+        /// <summary>
+        /// The possible styles for a font
+        /// Taken straight from iTextSharp
+        /// </summary>
+        [Flags]
+        public enum FontStyle
+        {
+            BOLD = 1,
+            BOLDITALIC = 3,
+            DEFAULTSIZE = 12,
+            ITALIC = 2,
+            NORMAL = 0,
+            STRIKETHRU = 8,
+            UNDEFINED = -1,
+            UNDERLINE = 4,
+        }
+
+        public enum FontFamily
+        {
+            Arial
+        }
+
+        public enum LabelStyle
+        {
+            L5160,
+            L5162,
+            L5260,
+            L5262,
+            L5520,
+            L5522,
+            L5660,
+            L5810,
+            L5960,
+            L5962,
+            L5970,
+            L5971,
+            L5972,
+            L5979,
+            L5980,
+            L7160,
+            L7654,
+            L7656,
+            L8160,
+            L8162,
+            L8252,
+            L8460,
+            L8462,
+            L8660,
+            L8662,
+            L8810
+        }
+    }
+}
---
+++ SharpPDFLabel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace SharpPDFLabel
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create the required label
+            var label = new SharpPDFLabel.Labels.A4Labels.Avery.L5160();
+            label.LabelsPerRow = 3;
+            label.LabelRowsPerPage = 10;
+            // Create a LabelCreator, passing the required label
+            var labelCreator = new SharpPDFLabel.LabelCreator(label);
+            var myImageAsAStream = new System.IO.MemoryStream(System.IO.File.ReadAllBytes("C:\\Users\\Vision-039\\Pictures\\Screenpresso\\2022-10-17_11h51_16.png"));
+            labelCreator.AddImage(myImageAsAStream);
+            labelCreator.AddText("Some Text", "Verdana", 10, embedFont: true);
+            labelCreator.AddText("Some more text with bold and underlined text", "Verdana", 10, true, SharpPDFLabel.Enums.FontStyle.BOLD, SharpPDFLabel.Enums.FontStyle.UNDERLINE);
+            labelCreator.IncludeLabelBorders = true;
+
+            //Create the PDF as a stream
+            var pdfStream = labelCreator.CreatePDF();
+            SaveFileStream("E:\\Demo\\Sample.pdf", pdfStream);
+            Console.WriteLine("PDF Generated");
+            Console.ReadKey();
+            //Do something with it!
+            //Response.AddHeader("Content-Disposition", "attachment; filename=sheet_of_labels.pdf");
+            //return new FileStreamResult(pdfStream, "application/pdf");
+        }
+
+        private static void SaveFileStream(string path, Stream stream)
+        {
+            var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+            stream.CopyTo(fileStream);
+            fileStream.Dispose();
+        }
+    }
+}
